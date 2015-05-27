93827f6d0679765535a6786b9c58eba5f3160846
Marcus Ruddick <ruddickmg@gmail.com>
2015-05-26 22:56:13 -0700

    added sockets port

--- public/js/js-wars.js
+++ public/js/js-wars.js
@@ -4,7 +4,7 @@
     
 \* ---------------------------------------------------------------------------------------------------------*/
 
-var socket = io();
+var socket = io('http://jswars-jswars.rhcloud.com/:8000');
 
 // Add a connect listener
 socket.on('connect',function() {
