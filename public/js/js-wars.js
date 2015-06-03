68eebb28c5fb4f471a497e47b62cfa77e7d850b0
Marcus Ruddick <ruddickmg@gmail.com>
2015-06-03 11:44:47 -0700

    ;lm;l

--- public/js/js-wars.js
+++ public/js/js-wars.js
@@ -1933,7 +1933,7 @@ app.display = function () {
         } else if (key.left in app.keys){
             app.temp.horizon = 'left';
             undo(key.left);
-        }else if(key.right in app.keys){
+        } else if(key.right in app.keys){
             app.temp.horizon = 'right';
             undo(key.right);
         }
@@ -1942,7 +1942,9 @@ app.display = function () {
 
     // find each element by their tag name, get the element that matches the currently selected index and return it
     var findElementByTag = function (tag, element, index) {
+        console.log('tag: '+tag+', index: '+index+',element: '+element);
         for (var e = 0; e < len; e += 1) {
+            console.log(element[e]);
             // element returns a string, so must cast the index to string for comparison
             // if the element tag value ( index ) is equal to the currently selected index then return it
             if (element[e].getAttribute(tag) === index.toString()) {
