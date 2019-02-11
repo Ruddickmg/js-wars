0267ec821865204a572d6383806f4c0dcbec3030
Marcus Ruddick <ruddickmg@gmail.com>
2019-02-11 11:59:37 -0800

    fixed types for "request-promise-native" in backend.ts

--- front/javascript/src/server/connections/backend.ts
+++ front/javascript/src/server/connections/backend.ts
@@ -1,3 +1,4 @@
+import {RequestPromise} from "@types/request-promise-native";
 import request = require("request-promise-native");
 import {Game} from "../../game/game";
 import {Map} from "../../game/map/map";
@@ -14,16 +15,16 @@ interface Parameters {
 }
 
 export interface Backend {
-  getLogin(password: string, email: string, database?: string): Promise<any>;
-  getUser(origin: string, id: BackendId, database?: string): Promise<any>;
-  getMaps(category: string, database?: string): Promise<any>;
-  getGames(id: BackendId, database?: string): Promise<any>;
-  saveGame(game: Game, database?: string): Promise<any>;
-  saveUser(user: User, database?: string): Promise<any>;
-  saveMap(map: Map, database?: string): Promise<any>;
-  deleteUser(id: BackendId, database?: string): Promise<any>;
-  deleteGame(id: BackendId, database?: string): Promise<any>;
-  deleteMap(id: BackendId, database?: string): Promise<any>;
+  getLogin(password: string, email: string, database?: string): RequestPromise<any>;
+  getUser(origin: string, id: BackendId, database?: string): RequestPromise<any>;
+  getMaps(category: string, database?: string): RequestPromise<any>;
+  getGames(id: BackendId, database?: string): RequestPromise<any>;
+  saveGame(game: Game, database?: string): RequestPromise<any>;
+  saveUser(user: User, database?: string): RequestPromise<any>;
+  saveMap(map: Map, database?: string): RequestPromise<any>;
+  deleteUser(id: BackendId, database?: string): RequestPromise<any>;
+  deleteGame(id: BackendId, database?: string): RequestPromise<any>;
+  deleteMap(id: BackendId, database?: string): RequestPromise<any>;
   migrate(database?: string): Promise<any>;
 }
 
@@ -32,17 +33,17 @@ export default single<Backend>(function(url: string): Backend {
   const json: boolean = true;
   const addToUrl = formatUrl(url);
   const combine = (...objects: object[]) => Object.assign({}, ...objects);
-  const del = (path: string, input: any): Promise<any> => makeRequest("DELETE", path, input);
-  const deleteGame = (game_id: BackendId, database?: string): Promise<any> => del("games", {game_id, database});
-  const deleteMap = (map_id: BackendId, database?: string): Promise<any> => del("maps", {map_id, database});
-  const deleteUser = (user_id: BackendId, database?: string): Promise<any> => del("users", {user_id, database});
-  const get = (path: string, input?: any): Promise<any> => makeRequest("GET", path, input);
-  const getGames = (user_id: BackendId, database?: string): Promise<any> => get("games", {user_id, database});
-  const getLogin = (email: string, password: string, database?: string): Promise<any> => {
+  const del = (path: string, input: any): RequestPromise<any> => makeRequest("DELETE", path, input);
+  const deleteGame = (game_id: BackendId, database?: string): RequestPromise<any> => del("games", {game_id, database});
+  const deleteMap = (map_id: BackendId, database?: string): RequestPromise<any> => del("maps", {map_id, database});
+  const deleteUser = (user_id: BackendId, database?: string): RequestPromise<any> => del("users", {user_id, database});
+  const get = (path: string, input?: any): RequestPromise<any> => makeRequest("GET", path, input);
+  const getGames = (user_id: BackendId, database?: string): RequestPromise<any> => get("games", {user_id, database});
+  const getLogin = (email: string, password: string, database?: string): RequestPromise<any> => {
     return get("login", {email, password, database});
   };
-  const getMaps = (category: string, database?: string): Promise<any> => get("maps", {category, database});
-  const getUser = (origin: string, user_id: BackendId, database?: string): Promise<any> => {
+  const getMaps = (category: string, database?: string): RequestPromise<any> => get("maps", {category, database});
+  const getUser = (origin: string, user_id: BackendId, database?: string): RequestPromise<any> => {
     return get("users", {origin, user_id, database});
   };
 
@@ -51,7 +52,7 @@ export default single<Backend>(function(url: string): Backend {
     path: string = "",
     input?: any,
     headers: object = {},
-  ): Promise<any> => {
+  ): RequestPromise<any> => {
     const uri: string = addToUrl(`/${path}`, input);
     const requestParameters: Parameters = {json, method, uri};
     return request(combine(requestParameters, headers));
@@ -63,13 +64,13 @@ export default single<Backend>(function(url: string): Backend {
       Promise.reject(Error("Could not complete migration."));
   });
 
-  const post = (path: string, body: any, database?: string): Promise<any> => {
+  const post = (path: string, body: any, database?: string): RequestPromise<any> => {
     return makeRequest("POST", path, {database}, {body});
   };
 
-  const saveGame = (game: Game, database?: string): Promise<any> => post("games", game, database);
-  const saveMap = (map: Map, database?: string): Promise<any> => post("maps", map, database);
-  const saveUser = (user: User, database?: string): Promise<any> => post("users", user, database);
+  const saveGame = (game: Game, database?: string): RequestPromise<any> => post("games", game, database);
+  const saveMap = (map: Map, database?: string): RequestPromise<any> => post("maps", map, database);
+  const saveUser = (user: User, database?: string): RequestPromise<any> => post("users", user, database);
 
   return {
     deleteGame,
